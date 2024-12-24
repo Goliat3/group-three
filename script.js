21b1f5869838c9a22bdb42f1334b9e9f679fb494
Goliat3 <tangfil0407@gmail.com>
2024-12-24 20:55:07 +0900

    main

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function newproblem() {
-    
+    const Problem = s //문제 json으로 받아올 것
+    currentProblem = Problem
+    document.getElementById('problem').innerText = currentProblem //아무튼 새로운 문제 할당 function
 }
 function save() {
 
